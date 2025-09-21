9894b091a240fbc302951e6221e0517a68cbcdf9
Prem Karhale <premkarhale383@gmail.com>
2025-09-21 18:11:46 +0530

    Update panda3-describe.py

--- panda3-describe.py
+++ panda3-describe.py
@@ -7,15 +7,15 @@ data = {
     
     "Age": [28, 34, 25, 41, 29, 37, 31, 45, 26, 38],
     
-    "Salary": [152000, 78000, 45000, 295000, 58000, 82000, 7000, 105000, 148000, 89000],
+    "Salary": [52000, 78000, 45000, 95000, 58000, 12000, 60000, 35000, 48000, 89000],
     
     "Performance_Score": [87, 92, 78, 95, 83, 89, 91, 88, 76, 94]
 }
 df=pd.DataFrame(data)
 df.to_csv("Data of companies Emp",index=0) #save dataframe to the csv
-# print("\n",df.describe()) # gives the statistical analysis of the data 
+# print("\n",df.describe()) # gives the statistical analysis of the data (mean , std , min , max )
 
-# minimum the standared deviation more is the consistency 
+# minimum the standared deviation more is the consistency  !!
 
 print(df)
 print(f"shape:{df.shape}")
