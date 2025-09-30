08dd6e99d73ca1351976bd2c7d6b7851a1c7b49f
Prem Karhale <premkarhale383@gmail.com>
2025-09-30 22:59:35 +0530

    Update concatination.py

--- concatination.py
+++ concatination.py
@@ -15,5 +15,6 @@ df_region2 =pd.DataFrame({
 # df_concate = pd.concat([df_region1,df_region2] ,ignore_index=True)
 
 # rowise axis = 1
+
 df_concate = pd.concat([df_region1,df_region2] , axis = 1 , ignore_index=True)
 print(df_concate)
