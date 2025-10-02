87aab49b8ffabe5e9a2ecd25edcbe785b5459798
Prem Karhale <premkarhale383@gmail.com>
2025-10-02 10:39:09 +0530

    Project 1

--- Project 1 (Data Cleaning )/kidneydata.py
+++ Project 1 (Data Cleaning )/kidneydata.py
@@ -15,7 +15,8 @@ j) Create subsets as per the given instructions
 k) divide the dataset into training (75%) and testing (25%).
 '''
 import pandas as pd 
-
+import seaborn as sns
+import matplotlib.pyplot as plt 
 df = pd.read_csv("sales_data_sample.csv",encoding="latin1")
 
 # print(df.shape) 
@@ -23,16 +24,22 @@ df = pd.read_csv("sales_data_sample.csv",encoding="latin1")
 col = df.columns
 print(col.dtype) #object
 
+df.isnull().sum
+zeros_count = (df==0).sum()   # () used for the condition purpose 
+print(zeros_count)
 
 print(df.mean(numeric_only=True))
 
+remove_all_null_values = df.fillna(df.mode().iloc[0] )
+print(remove_all_null_values.isnull().sum().sum()) #0
+
+# fillna specific col wise 
 # df["POSTALCODE"].fillna(df["POSTALCODE"].mode()[0] , inplace=True)
 # print(df["POSTALCODE"].isnull().sum())
 
-# print(df["POSTALCODE"])
 
-remove_all_null_values = df.fillna(df.mode().iloc[0] )
-print(remove_all_null_values.isnull().sum().sum()) #0
+sns.pairplot(df)
+plt.show()
 
 
 
