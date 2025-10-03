ff03ad0801e3962b3345027a067f206e8f684a64
Prem Karhale <premkarhale383@gmail.com>
2025-10-03 22:05:13 +0530

    project 1 (Data cleaning )

--- Project 1 (Data Cleaning )/kidneydata.py
+++ Project 1 (Data Cleaning )/kidneydata.py
@@ -16,30 +16,53 @@ k) divide the dataset into training (75%) and testing (25%).
 '''
 import pandas as pd 
 import seaborn as sns
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
 df = pd.read_csv("sales_data_sample.csv",encoding="latin1")
 
 # print(df.shape) 
 # print(df.describe())
-col = df.columns
-print(col.dtype) #object
+# col = df.columns
+# print(col.dtype) #object
 
-df.isnull().sum
-zeros_count = (df==0).sum()   # () used for the condition purpose 
-print(zeros_count)
+# df.isnull().sum
+# zeros_count = (df==0).sum()   # () used for the condition purpose 
+# print(zeros_count)
 
-print(df.mean(numeric_only=True))
+# print(df.mean(numeric_only=True))
 
+# METHOD TO REMOVE ALL THE NULL\NAN VALUES FROM THE DATASET 
 remove_all_null_values = df.fillna(df.mode().iloc[0] )
 print(remove_all_null_values.isnull().sum().sum()) #0
+df = remove_all_null_values
+print(df.isnull().sum()) #0
 
 # fillna specific col wise 
 # df["POSTALCODE"].fillna(df["POSTALCODE"].mode()[0] , inplace=True)
 # print(df["POSTALCODE"].isnull().sum())
 
 
-sns.pairplot(df)
-plt.show()
+# sns.pairplot(df)
+# plt.show()
+
+#unwanted features ! data cleaning 
+df = df.drop(columns=['ORDERNUMBER', 'ORDERDATE', 'PRODUCTCODE', 
+    'CUSTOMERNAME', 'PHONE', 'ADDRESSLINE1', 'ADDRESSLINE2',
+    'CONTACTLASTNAME', 'CONTACTFIRSTNAME'])
+
+x=df.drop(columns= ['SALES'] , axis= 1) # all the df other than sales 
+y= df['SALES'] # contain only sales data
+
+x_encoded = pd.get_dummies(x , drop_first=True) # Convert categorical to numeric
+# Split 
+x_train , x_test , y_train , y_test = train_test_split(x_encoded , y , test_size=0.25 , random_state=42)
+# print(x_train)
+# print(x_test)
+# print(y_train)
+# print(y_test)
+
+
 
 
 
